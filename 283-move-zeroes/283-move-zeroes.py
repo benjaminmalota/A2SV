88cc59ed45e83b4ce138cd16047b9ce8d520c89a
Binyam Malota <68713987+benjaminmalota@users.noreply.github.com>
2022-10-01 18:55:44 +0300

    Time: 188 ms (86.89%), Space: 15.6 MB (16.66%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,10 +4,12 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         p1 = 0
+        p2 = len(nums)
         
-        for p2 in range(len(nums)):
-            if nums[p2]!=0:
-                nums[p1], nums[p2] = nums[p2], nums[p1]
-                p1 +=1
-        return nums
-            
+        while p1 < p2:
+            if nums[p1] == 0:
+                nums.append(nums.pop(p1))
+                p2 -= 1
+            else:
+                p1 += 1
+        
